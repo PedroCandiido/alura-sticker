aba2ef31b5688a07d6c3ffcb56658c64dbe3f6b6
PedroCandiido <116935967+PedroCandiido@users.noreply.github.com>
2023-03-28 16:38:42 -0300

    Aula01-ConsumindoAPI

---
+++ src/App.java
@@ -0,0 +1,44 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.Builder;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // Realizar a conexão HTTP e buscar o top 250 filmes
+
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // exibindo o conteudo do body
+        //System.out.println(body);
+        
+        // Filtrar somente os dados que interessam (Titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
+        //mostra o tamanho da lista
+        //System.out.println(listaDeFilmes.size());
+        
+        //mostra algum item especifico da lista que eu selecionar
+        //System.out.println(listaDeFilmes.get(0));
+        
+        // Exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+    }
+}
